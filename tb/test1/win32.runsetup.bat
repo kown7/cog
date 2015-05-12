bbe5b3cd82a926cca7e475cb1cd3a1c5fef92928
Kristoffer Nordström <k.nordstroem@helveting.ch>
2015-05-12 12:46:09 +0200

    Adapted scripts to unit-test's changes.

--- tb/test1/win32.runsetup.bat
+++ tb/test1/win32.runsetup.bat
@@ -1,2 +1,2 @@
-..\cog\setup.py ..\..\src tb_test1.sv E:\modeltech_pe_10.4a\win32pe\
+..\cog\createSimFolder.py ..\..\src tb_test1.sv E:\modeltech_pe_10.4a\win32pe\
 
--- tb/testCW/cygwin.setup.sh
+++ tb/testCW/cygwin.setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-../cog/setup.py ../../src/ tb_test1.sv /cygdrive/e/modeltech_pe_10.4a/win32pe/
+../cog/createSimFolder.py ../../src/ tb_test1.sv /cygdrive/e/modeltech_pe_10.4a/win32pe/
 
